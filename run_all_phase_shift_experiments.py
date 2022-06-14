e677c6011d39012455a899f25769b24e65410c17
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2022-06-14 12:19:01 -0400

    Add support for multiple seeds

--- run_all_phase_shift_experiments.py
+++ run_all_phase_shift_experiments.py
@@ -1,15 +1,21 @@
-import os
+import random
 from pathlib import Path
 
+random.seed(42)
+SEEDS = [random.randint(1, 1e6) for _ in range(5)]
+print("Seeds:", SEEDS)
 DATASETS = ["mrpc", "rte", "cola", "boolq", "cb"]
 MODELS = [
-    ("gpt2", "EleutherAI/gpt-neo-125M"),
-    ("gpt2", "EleutherAI/gpt-neo-1.3B"),
-    ("gpt2", "EleutherAI/gpt-neo-2.7B"),
+    # ("gpt2", "EleutherAI/gpt-neo-125M"),
+    ("opt", "facebook/opt-125m"),
+    # ("gpt2", "EleutherAI/gpt-neo-1.3B"),
+    ("opt", "facebook/opt-350m"),
+    # ("gpt2", "EleutherAI/gpt-neo-2.7B"),
+    ("opt", "facebook/opt-2.7b"),
     ("opt", "facebook/opt-13b"),
     ("opt", "facebook/opt-30b"),
 ]
-PHASE_SHIFTS = list(range(0, 700, 100))
+PHASE_SHIFTS = list(range(0, 1001, 100))
 FEW_SHOTS = [0, 5]
 
 import os
@@ -19,37 +25,37 @@ if __name__ == "__main__":
     results_dir = Path("results")
     results_dir.mkdir(parents=True, exist_ok=True)
 
-    for ds in DATASETS:
-        for model, model_name in MODELS:
-            for shot in FEW_SHOTS:
-                for ps in PHASE_SHIFTS:
-                    output_path = (
-                        results_dir / f"{ds}__{model}__{shot}__{model_name}__{ps}.json".replace("EleutherAI/", "").replace("facebook/", "")
-                    )
-                    print(f"Running results for {output_path}")
-                    if output_path.exists():
-                        print("Skipped.")
-                        continue
+    for seed in SEEDS:
+        for ds in DATASETS:
+            for model, model_name in MODELS:
+                for shot in FEW_SHOTS:
+                    for ps in PHASE_SHIFTS:
+                        exp_name = f"{seed}__{ds}__{model_name}__{shot}__{ps}"
+                        exp_name = exp_name.replace("EleutherAI/", "").replace(
+                            "facebook/", ""
+                        )
+                        output_path = results_dir / f"{exp_name}.json"
+                        print(f"Running results for {output_path}")
+                        if output_path.exists():
+                            print("Skipped.")
+                            continue
 
-                    batch_size = (
-                        8
-                        if model_name in ["gpt2-base", "gpt2-medium", "gpt2-large"]
-                        else 1
-                    )
-                    python_run_script = (
-                        f"python main.py "
-                        f"--model {model}-ps "
-                        f"--model_args pretrained={model_name},phase_shift={ps} "
-                        f"--tasks {ds} "
-                        f"--num_fewshot {shot} "
-                        f"--batch_size {batch_size} "
-                        f"--output_path {output_path} "
-                        f"--no_cache "
-                    )
-                    print(python_run_script)
-                    try:
-                        os.system(python_run_script)
-                        time.sleep(5)
-                    except Exception as exp:
-                        print(exp)
-                        exit(1)
+                        batch_size = 1
+                        python_run_script = (
+                            f"python main.py "
+                            f"--model {model}-ps "
+                            f"--model_args pretrained={model_name},phase_shift={ps} "
+                            f"--tasks {ds} "
+                            f"--num_fewshot {shot} "
+                            f"--batch_size {batch_size} "
+                            f"--output_path {output_path} "
+                            f"--seed {seed} "
+                            f"--no_cache "
+                        )
+                        print(python_run_script)
+                        try:
+                            os.system(python_run_script)
+                            time.sleep(5)
+                        except Exception as exp:
+                            print(exp)
+                            exit(1)
